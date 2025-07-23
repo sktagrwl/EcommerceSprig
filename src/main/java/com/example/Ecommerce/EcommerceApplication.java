3370ec7c830fa7fa77121ebff64e7b44cb20cbd3
sktagrwl <agarrwaldevansh23@gmail.com>
2025-07-23 20:16:35 +0530

    Configured Netflix Eureka Client to create client registy

--- src/main/java/com/example/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/Ecommerce/EcommerceApplication.java
@@ -4,10 +4,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
